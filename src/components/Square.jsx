8f4c1ceec91579c9aea5896301920ed2c0b6b609
chakshu <chakshushandilya@gmail.com>
2024-02-20 16:10:52 +0530

    added sass

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const Square = ({ value }) => {
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
+};
+
+export default Square;
