a90c9a745076a61cc77f8c870e3e2088fb255aa0
chakshu <chakshushandilya@gmail.com>
2024-02-21 10:33:29 +0530

    displayed X

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
